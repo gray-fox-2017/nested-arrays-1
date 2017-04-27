cafe7493a74079dcf684cf666cfd55cffd7315ba
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-04-27 11:13:42 +0700

    no bug detected

--- chessboard.js
+++ chessboard.js
@@ -2,13 +2,68 @@
 
 const makeChessboard = () => {
   let chessboard = []
+  /*
+  Posisi [0][0] adalah Benteng Hitam sebelah kanan dari pemain Hitam
+  atau sebelah kiri pemain Putih
+  */
 
-  // ... write your code here
+  var isi = ["Benteng","Kuda","Gajah","Raja","Ratu","Gajah","Kuda","Benteng", "Prajurit", "", "Hitam", "Putih"];
+  var baris_0 = [];
+  var baris_1 = [];
+  var baris_kosong = [];
+  var baris_6 = [];
+  var baris_7 = [];
+  for (let i = 0; i < 8; i++){
+    switch (i) {
+      case 0:
+      for (let j = 0, k = 8; j < k;j++){
+        baris_0.push(isi[j]+ " " + isi[10]);
+      }
+      chessboard.push(baris_0);
+        break;
+
+        case 1:
+        for (let j = 0, k = 8; j < k;j++){
+          baris_1.push(isi[8]+ " " + isi[10]);
+        }
+        chessboard.push(baris_1);
+          break;
+
+        case 5:
+          for (let j = 0, k = 8; j < k;j++){
+            baris_kosong.push(isi[9]);
+          }
+          for (let l = 0; l < 4 ; l++){
+            chessboard.push(baris_kosong);
+          }
+          break;
+
+        case 6:
+          for (let j = 0, k = 8; j < k;j++){
+            baris_6.push(isi[8]+ " " + isi[11]);
+          }
+          chessboard.push(baris_6);
+            break;
+
+          case 7:
+            for (let j = 0, k = 8; j < k;j++){
+              baris_7.push(isi[j]+ " " + isi[11]);
+            }
+            chessboard.push(baris_7);
+              break;
+
+      default:
+      "";
+    }
+
+  }
+
   return chessboard
 }
-
 const printBoard = x => {
-  // ... write your code here
+  for (let i = 0 ; i < x.length; i++){
+    console.log(x[i].join(" "))
+  }
 }
 
 printBoard(makeChessboard())
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,38 @@
 function tic_tac_toe_board () {
-  // ...
+  let chooser = Math.ceil(Math.random()*2);
+  let X = 0;
+  let Y = 0;
+  let tampung = [];
+  let board = [];
+  let stop = false;
+
+  if (chooser === 1){
+    X = 4;
+    Y = 5;
+  } else if (chooser === 2){
+    X = 5;
+    Y = 4;
+  }
+
+  while (stop === false){
+    let X_or_Y = Math.ceil(Math.random()*2);
+    if (X_or_Y === 1 && X > 0){
+      tampung.push("X");
+      X--;
+    } else if (X_or_Y === 2 && Y > 0){
+      tampung.push("Y");
+      Y--;
+    } else if (X == 0 && Y == 0){
+      stop = true
+    }
+  }
+
+  while (tampung.length > 0){
+    board.push(tampung.splice(0,3));
+    board.push('--------------');
+  }
+
+  return board;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
