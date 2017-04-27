0d37425d205bc53f1c7dc376051b03e0da7af95b
hakiemaul <noersedya@gmail.com>
2017-04-27 11:49:23 +0700

    nested array 1 done

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,38 @@
 'use strict'
+var pieces = ['Bidak', 'Benteng', 'Kuda', 'Gajah', 'Menteri', 'Raja'];
+var side = ['Hitam', 'Putih'];
 
 const makeChessboard = () => {
-  let chessboard = []
+  let chessBoard = []
+  for(let i = 0; i < 8; i++) {
+    chessBoard[i] = [];
+    for(let j = 0; j < 8; j++) {
+      chessBoard[i].push('');
+    }
+  }
 
-  // ... write your code here
-  return chessboard
+  for(let j = 0; j < 2; j++) {
+    let letakBidak = j + 1;
+    let letakPerwira = j;
+    if(j == 1) {
+      letakBidak = 6;
+      letakPerwira = 7;
+    }
+    for(let i = 0; i < 8; i++) {
+      chessBoard[letakBidak][i] = pieces[0] + ' ' + side[j];
+      if(i <= 4) {
+        chessBoard[letakPerwira][i] = pieces[i+1] + ' ' + side[j];
+      }
+      if(i > 4) {
+        chessBoard[letakPerwira][i] = pieces[8-i] + ' ' + side[j];
+      }
+    }
+  }
+  return chessBoard
 }
 
 const printBoard = x => {
-  // ... write your code here
+  console.log(x)
 }
 
 printBoard(makeChessboard())
