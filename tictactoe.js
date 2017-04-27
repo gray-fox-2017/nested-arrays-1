1159279bcf0af8876bbd90f15da776bc993ff02e
daniarmeiradi <mailtoreidy@gmail.com>
2017-04-27 15:03:16 +0700

    benerin bug cuy

--- tictactoe.js
+++ tictactoe.js
@@ -1,28 +1,43 @@
 function tic_tac_toe_board () {
   // ...
   let ticTac = [];
-  for (let i = 0; i<5; i++) {
+  let countX = 0;
+  let countO = 0;
+
+  for (let i = 0; i<9; i++) {
     generator();
   }
-  for (let i = 0; i<4; i++) {
-    let gabung = ticTac.join('');
-    let jumlahX = gabung.match(/[x]/g);
-    let jumlahO = gabung.match(/[o]/g);
-    if (jumlahO.length === jumlahX.length) {
-      generator();
-    } else if (jumlahO.length  < jumlahX.length) {
-      ticTac.push('o');
-    } else if (jumlahO.length > jumlahX.length ){
-      ticTac.push('x');
-    }
-  }
+  // for (let i = 0; i<4; i++) {
+  //   let gabung = ticTac.join('');
+  //   let jumlahX = gabung.match(/[x]/g);
+  //   let jumlahO = gabung.match(/[o]/g);
+  //   if (jumlahO.length === jumlahX.length) {
+  //     generator();
+  //   } else if (jumlahO.length  < jumlahX.length) {
+  //     ticTac.push('o');
+  //   } else if (jumlahO.length > jumlahX.length ){
+  //     ticTac.push('x');
+  //   }
+  // }
 
   function generator() {
-    let generator = Math.floor(Math.random()*10);
-    if (generator % 2 === 0) {
-      return ticTac.push('x')
+    let rand = Math.floor(Math.random()*10);
+    if (rand % 2 === 0 ) {
+      if (countX < 5) {
+        ticTac.push('x')
+        countX++;
+      } else {
+        ticTac.push('o')
+        countO++;
+      }
     } else {
-      return ticTac.push('o')
+      if (countO < 5) {
+        ticTac.push('o')
+        countO++;
+      } else {
+        ticTac.push('x')
+        countX++;
+      }
     }
   }
 
