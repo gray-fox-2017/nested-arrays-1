f445a51c5a2b4bdb7172b8d135dd900d87ec933b
daniarmeiradi <mailtoreidy@gmail.com>
2017-04-27 11:16:06 +0700

    ganti generator jd fungsi

--- tictactoe.js
+++ tictactoe.js
@@ -2,31 +2,30 @@ function tic_tac_toe_board () {
   // ...
   let ticTac = [];
   for (let i = 0; i<5; i++) {
-    let generator = Math.floor(Math.random()*10);
-    if (generator % 2 === 0) {
-      ticTac.push('x')
-    } else {
-      ticTac.push('o')
-    }
+    generator();
   }
   for (let i = 0; i<4; i++) {
     let gabung = ticTac.join('');
     let jumlahX = gabung.match(/[x]/g);
     let jumlahO = gabung.match(/[o]/g);
     if (jumlahO.length === jumlahX.length) {
-      let generator = Math.floor(Math.random()*10);
-      if (generator % 2 === 0) {
-        ticTac.push('x')
-      } else {
-        ticTac.push('o')
-      }
+      generator();
     } else if (jumlahO.length  < jumlahX.length) {
       ticTac.push('o');
     } else if (jumlahO.length > jumlahX.length ){
       ticTac.push('x');
     }
-
   }
+
+  function generator() {
+    let generator = Math.floor(Math.random()*10);
+    if (generator % 2 === 0) {
+      return ticTac.push('x')
+    } else {
+      return ticTac.push('o')
+    }
+  }
+
   var ticTacBoard = [];
   for (i=0; i<ticTac.length;i+=3) {
     ticTacBoard.push(ticTac.slice(i, i+3))
