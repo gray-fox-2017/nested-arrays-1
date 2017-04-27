94c7a153f78e6a7babd16d4b4e2a0ea972e49a93
ingelieur <ingelieur@gmail.com>
2017-04-27 12:04:55 +0700

    lieur.out

--- chessboard.js
+++ chessboard.js
@@ -2,9 +2,10 @@
 
 const makeChessboard = () => {
   let chessboard=[[]];
-  let chesspiece=[['Benteng', 'Kuda', 'Gajah','Raja','Ratu'],[]];
-  for (let i=0;i<3;i++) {chesspiece[0].push(chesspiece[0][2-i]);}
-  for (let j=0;j<8;j++) {chesspiece[1].push("Bidak");}
+  let chesspiece=[['Benteng', 'Kuda', 'Gajah','Raja','Ratu','Gajah','Kuda','Benteng'],[]];
+  //let chesspiece=[['Benteng', 'Kuda', 'Gajah','Raja','Ratu'],[]];
+  //for (let i=0;i<3;i++) {chesspiece[0].push(chesspiece[0][2-i]);}
+  for (let j=0;j<8;j++) {chesspiece[1].push("Pion");}
   
   // make empty chessboard
   for (let i=0;i<8;i++){
@@ -16,10 +17,10 @@ const makeChessboard = () => {
 
   // fill the chessboard
   for (let pos=0;pos<8;pos++) {
-    chessboard[0][pos] = chesspiece[0][7-pos];
-    chessboard[7][pos] = chesspiece[0][pos];
-    chessboard[1][pos] = chesspiece[1][pos];
-    chessboard[6][pos] = chesspiece[1][pos];
+    chessboard[0][pos] = chesspiece[0][7-pos] + " Hitam";
+    chessboard[7][pos] = chesspiece[0][pos] + " Putih";
+    chessboard[1][pos] = chesspiece[1][pos] + " Hitam"; 
+    chessboard[6][pos] = chesspiece[1][pos] + "Putih";
   }
 
   return chessboard
