cd6d78e620add52bed246b05ae651c901847dc50
ppsari <poppymighty@gmail.com>
2017-04-27 17:23:01 +0700

    ralat

--- tictactoe.js
+++ tictactoe.js
@@ -1,25 +1,19 @@
 function tic_tac_toe_board () {
-  //eks = 1, oh = 0
-  let nEx = 0;
-  let nOh = 0;
   let board = [];
+  let data = [['O',0],['X',0]];
 
   for (let i = 0; i < 3 ; i++) {
     let baris = [];
     for (let j = 0; j < 3; j++) {
       let res = Math.round(Math.random());
-      if (res === 0 && nOh < 5){
-        nOh += 1;
-        baris.push('O');
-      }else {
-        nEx +=1;
-        baris.push('X');
-      }
+      if (data[res][1] > 4 ) res = (res === 1 ? 0 : 1);
+      data[res][1] += 1;
+      baris.push(data[res][0]);
     }
     board.push(baris);
   }
+  
   return board;
-
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
