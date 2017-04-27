7266dd4cce3079a5ba2065261164e594fe61ef01
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-04-27 12:16:02 +0700

    creating code for chessboard and tictactoe

--- chessboard.js
+++ chessboard.js
@@ -2,13 +2,33 @@
 
 const makeChessboard = () => {
   let chessboard = []
-
   // ... write your code here
+  var bidakMentri = ['Benteng', 'Kuda', 'Gajah', 'Mentri', 'Raja', 'Gajah', 'Kuda', 'Benteng'];
+  var pionHitam = [], bidakMentriHitam = [], bidakMentriPutih=[], pionPutih = [], spaces = [];
+  var putih = 'putih', pion = 'Pion', hitam = 'hitam', space = 'kosong';
+  for (let i=0; i<bidakMentri.length; i++){
+    pionHitam.push(pion+' '+hitam);
+    pionPutih.push(pion+' '+putih);
+    bidakMentriHitam.push(bidakMentri[i]+' '+hitam);
+    bidakMentriPutih.push(bidakMentri[i]+' '+putih);
+    spaces.push(space);
+  }
+  for (let i=0; i<4; i++){
+    chessboard.push(spaces);
+  }
+  chessboard.unshift(pionPutih);
+  chessboard.unshift(bidakMentriPutih);
+  chessboard.push(pionHitam);
+  chessboard.push(bidakMentriHitam);
   return chessboard
 }
 
 const printBoard = x => {
   // ... write your code here
+  var print = [];
+  for (let i=0; i<8; i++){
+    console.log(`Papan ke ${i+1} : ${x[i].join(' | ')}`);
+  }
 }
 
 printBoard(makeChessboard())
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,23 @@
 function tic_tac_toe_board () {
   // ...
+  let board = [];
+  let length_board = 3;
+  let Ow = (Math.round(Math.random()) === 0) ? 5:4;
+  let eX = 9-Ow;
+  for (let i=0; i<length_board; i++){
+    board.push([]);
+    for (let j=0; j<length_board; j++){
+      if(Math.round(Math.random())===0){
+        value = Ow > 0 ? 'O' : 'X';
+        Ow--;
+      }else {
+        value = eX > 0 ? 'X' : 'O';
+        eX--;
+      }
+      board[i].push(value);
+    }
+  }
+  return board;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
