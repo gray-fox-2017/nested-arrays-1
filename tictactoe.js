7d33cab45a1c907634d3a715668c823a91e423bf
AndrewSenewe <sindrew.christ@gmail.com>
2017-04-27 15:43:33 +0700

    TicTacTou Done, NestedArray completed

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,34 @@
 function tic_tac_toe_board () {
+  var hitX = 0;
+  var hitO = 0;
   // ...
+  var board = [];
+  for (let i = 0; i<3; i++) {
+    board.push(["","",""]);
+  }
+  for (let j=0; j<3; j++) {
+    for (let k=0; k<3; k++) {
+      var ran = Math.floor(Math.random()*2);
+      // console.log(ran);
+      if (ran == 0) {
+        if (hitX < 5) {
+          board[j][k] = "X";
+          hitX++;
+        } else {
+          board[j][k] = "O";
+        }
+      } else {
+        if (hitO < 5) {
+          board[j][k] = "O";
+          hitO++;
+        } else {
+          board[j][k] = "X";
+        }
+      }
+    }
+  }
+  return board;
 }
+
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
