96fc8daed03410fb05043b95bc4cbd62c2b23a4e
jamessraun <jeezybig@gmail.com>
2017-04-27 11:34:31 +0700

    nested_arrays_1 done

--- chessboard.js
+++ chessboard.js
@@ -2,13 +2,29 @@
 
 const makeChessboard = () => {
   let chessboard = []
+  let bidak_putih=['Benteng Putih','Kuda Putih','Peluncur Putih','Ratu Putih','Raja Putih','Peluncur Putih','Kuda Putih','Benteng Putih']
+  let pion_putih=['pion Putih','pion Putih','pion Putih','pion Putih','pion Putih','pion Putih','pion Putih','pion Putih']
+  let bidak_hitam=['Benteng Hitam','Kuda Hitam','Peluncur Hitam','Ratu Hitam','Raja Hitam','Peluncur Hitam','Kuda Hitam','Benteng Hitam']
+  let pion_hitam=['pion Hitam','pion Hitam','pion Hitam','pion Hitam','pion Hitam','pion Hitam','pion Hitam','pion Hitam']
 
-  // ... write your code here
+  for(let i=0;i<8;i++)
+  {
+    if(i<4){
+      if(i===0)
+      chessboard.push(bidak_putih);
+      else chessboard.push(pion_putih);
+    }else{
+      if(i===7)
+      chessboard.push(bidak_hitam);
+      else chessboard.push(pion_hitam);
+    }
+  }
   return chessboard
 }
 
 const printBoard = x => {
   // ... write your code here
+  console.log(x)
 }
 
 printBoard(makeChessboard())
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,30 @@
 function tic_tac_toe_board () {
   // ...
+  let arr=[];
+  let board=[];
+  let random_number = Math.floor(Math.random()*10);
+
+  if(random_number%2===0){
+    for(let i=1; i<=9; i++){
+      if(i%2===0)
+        arr.push('X');
+      else arr.push('O');
+    }
+  }else{
+    for(let i=1; i<=9; i++){
+      if(i%2===0)
+        arr.push('O');
+      else arr.push('X');
+    }
+  }
+
+
+    for(let i=0;i<9;i+=3){
+      board.push(arr.slice(i,i+3));
+    }
+
+  return board;
+
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
