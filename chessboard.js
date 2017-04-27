3cffbbe29d098bdf0aa691ed6d5ad09199522901
ingelieur <ingelieur@gmail.com>
2017-04-27 11:20:51 +0700

    lieur.out

--- chessboard.js
+++ chessboard.js
@@ -1,14 +1,32 @@
 'use strict'
 
 const makeChessboard = () => {
-  let chessboard = []
+  let chessboard=[[]];
+  let chesspiece=[['Benteng', 'Kuda', 'Gajah','Raja','Ratu'],[]];
+  for (let i=0;i<3;i++) {chesspiece[0].push(chesspiece[0][2-i]);}
+  for (let j=0;j<8;j++) {chesspiece[1].push("Bidak");}
+  
+  // make empty chessboard
+  for (let i=0;i<8;i++){
+    if (i>0) { chessboard.push([]) }
+    for (let j=0;j<8;j++){
+      chessboard[i].push("");
+    }
+  }
 
-  // ... write your code here
+  // fill the chessboard
+  for (let pos=0;pos<8;pos++) {
+    chessboard[0][pos] = chesspiece[0][7-pos];
+    chessboard[7][pos] = chesspiece[0][pos];
+    chessboard[1][pos] = chesspiece[1][pos];
+    chessboard[6][pos] = chesspiece[1][pos];
+  }
+
   return chessboard
 }
 
 const printBoard = x => {
-  // ... write your code here
+  console.log(x);
 }
 
 printBoard(makeChessboard())
