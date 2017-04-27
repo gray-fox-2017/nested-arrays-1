bf98d222332b914b5724bc1f253357f18b975e90
daniarmeiradi <mailtoreidy@gmail.com>
2017-04-27 11:12:06 +0700

    kode selesasi, tapi masih belum bisa di turunin arraynya

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,40 @@
 function tic_tac_toe_board () {
   // ...
+  let ticTac = [];
+  for (let i = 0; i<5; i++) {
+    let generator = Math.floor(Math.random()*10);
+    if (generator % 2 === 0) {
+      ticTac.push('x')
+    } else {
+      ticTac.push('o')
+    }
+  }
+  for (let i = 0; i<4; i++) {
+    let gabung = ticTac.join('');
+    let jumlahX = gabung.match(/[x]/g);
+    let jumlahO = gabung.match(/[o]/g);
+    if (jumlahO.length === jumlahX.length) {
+      let generator = Math.floor(Math.random()*10);
+      if (generator % 2 === 0) {
+        ticTac.push('x')
+      } else {
+        ticTac.push('o')
+      }
+    } else if (jumlahO.length  < jumlahX.length) {
+      ticTac.push('o');
+    } else if (jumlahO.length > jumlahX.length ){
+      ticTac.push('x');
+    }
+
+  }
+  var ticTacBoard = [];
+  for (i=0; i<ticTac.length;i+=3) {
+    ticTacBoard.push(ticTac.slice(i, i+3))
+  }
+
+  return ticTacBoard;
 }
+
+
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
