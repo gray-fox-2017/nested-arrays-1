a13a37f565c12c14631470d237f5fd179843973c
fajarkarim <fajarabdulkarim@gmail.com>
2017-04-27 14:18:19 +0700

    updated pion

--- chessboard.js
+++ chessboard.js
@@ -11,10 +11,14 @@ const makeChessboard = () => {
   let whiteChess = [];
   let blackPion = [];
   let whitePion = [];
+  let kosong = [];
 
   for (let i = 0; i < figure.length; i++) {
     blackChess.push(figure[i] + "Hitam");
     whiteChess.push(figure[i] + "Putih");
+    blackPion.push("Pion Hitam");
+    whitePion.push("Pion Putih");
+    kosong.push("Kosong");
   }
 
   for (let i = 0; i < 8; i++) {
@@ -27,14 +31,15 @@ const makeChessboard = () => {
     } else if (i === 7) {
       finalArr.push(whiteChess.reverse());
     } else {
-      finalArr.push("Kosong");
+      finalArr.push(kosong);
     }
   }
-  return chessboard
+  return finalArr;
 }
 
 const printBoard = x => {
   // ... write your code here
+  console.log(x);
 }
 
 printBoard(makeChessboard())
