225872e818c40c676e155f489f0335adfcf96af0
1000pilar <nunua7a8@gmailcom>
2017-04-27 19:34:29 +0700

    chessboard & tictactoe Done

--- chessboard.js
+++ chessboard.js
@@ -2,13 +2,36 @@
 
 const makeChessboard = () => {
   let chessboard = []
+    let chess = ["Benteng ", "Kuda ", "Gajah ", "Menteri ", "Raja ", "Gajah ", "Kuda ", "Benteng "]
+    let color = ["Hitam", "Putih"];
 
-  // ... write your code here
-  return chessboard
+    // ... write your code here
+    for (let i = 0; i < chess.length; i++){
+      let newArr = [];
+      chessboard.push(newArr);
+      for(let j = 0; j < chess.length; j++){
+        if (i == 0){
+          chessboard[i].push(chess[j]+color[0])
+        }else if(i == 1){
+          chessboard[i].push("Bidak "+color[0])
+        }else if(i == 7){
+          chessboard[i].push(chess[j]+color[1])
+        }else if(i == 6){
+          chessboard[i].push("Bidak  "+color[1])
+        }else{
+          chessboard[i].push("___________")
+        }
+      }
+    }
+    return chessboard
 }
 
 const printBoard = x => {
   // ... write your code here
+  for(let i = 0; i < 8 ; i++){
+    console.log(`${i+1} ${x[i].join(" |")}`)
+
+  }
 }
 
 printBoard(makeChessboard())
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,25 @@
 function tic_tac_toe_board () {
   // ...
+  let result = [];
+  var boardsLength = 3;
+  let random_notation = (Math.round(Math.random() === 0)) ? 4 : 5;
+
+  let O_notation = 9-random_notation;
+  for(let i = 0; i < boardsLength ; i++){
+    let newArr = [];
+    result.push(newArr);
+    for(let j = 0; j < boardsLength ; j++){
+      if(Math.round(Math.random() === 0)){
+        let value =  random_notation > 0 ? "X" : "O";
+        random_notation--
+      }else{
+        value = O_notation > 0 ? "X" : "O";
+        O_notation--
+      }
+      result[i].push(value);
+    }
+  }
+  return result;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
