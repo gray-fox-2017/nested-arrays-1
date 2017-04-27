810c2ef4e442ab41ead38c08b231ffd4f6ffbab3
stedyyulius <stedyyulius@gmail.com>
2017-04-27 11:10:33 +0700

    boardku bagus

--- chessboard.js
+++ chessboard.js
@@ -2,13 +2,29 @@
 
 const makeChessboard = () => {
   let chessboard = []
+  let empty = ""
+  let chess = ["Benteng","Kuda","Luncur","Ster","Raja","Luncur","Kuda","Benteng"]
+  let pion = "Pion"
+  let color = ["Hitam","Putih"]
+  for (let i = 0; i < 8; i++){
+    chessboard.push([])
+  }
+  for (let i = 0; i < 8; i++){
+  chessboard[0].push(" " + chess[i] + " " + color[0])
+  chessboard[1].push(" " + pion + " " + color[0])
+  chessboard[2].push(empty)
+  chessboard[3].push(empty)
+  chessboard[4].push(empty)
+  chessboard[5].push(empty)
+  chessboard[6].push(" " + pion + " " + color[1])
+  chessboard[7].push(" " + chess[i] + " " + color[1])
+  }
 
-  // ... write your code here
   return chessboard
 }
 
 const printBoard = x => {
-  // ... write your code here
+console.log(x)
 }
 
 printBoard(makeChessboard())
--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,31 @@
 function tic_tac_toe_board () {
-  // ...
+  let o = 0
+  let x = 0
+  let board = []
+  let board2 = []
+  for (let i = 0; i < 3; i++){
+    board.push([])
+    for(let j = 0; j < 3; j++){
+      let random = Math.floor(Math.random()*2)
+        if(random === 0 && o < 5){
+          board[i].push("O")
+          o++
+        }
+        else if(x < 5){
+          board[i].push("X")
+          x++
+        }
+        else if (o < 5 && x === 5){
+          board[i].push("O")
+          o++
+        }
+    }
+  }
+  for(let x = 0; x < board.length; x++){
+    board2.push(board[x].join("|"))
+  }
+  return board2.join("\n")
 }
+
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
