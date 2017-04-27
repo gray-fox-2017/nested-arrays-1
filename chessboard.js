d3ab776e1a9b674baee5bd7f6bc13d5c060d10fd
ambodalle <ambodalle.st@gmail.com>
2017-04-27 21:16:22 +0700

    nested array 1 done

--- chessboard.js
+++ chessboard.js
@@ -4,11 +4,40 @@ const makeChessboard = () => {
   let chessboard = []
 
   // ... write your code here
+  let bidak = ["Benteng", "Kuda" , "Bhisop", "Raja", "Mentri", "Bishop" , "Kuda", "Benteng" ];
+  for (let i=0; i<bidak.length; i++) {
+    chessboard.push([]);
+    for (let j=0; j< bidak.length; j++) {
+      if (i==0) {
+        chessboard[i].push(`${bidak[j]} Hitam`);
+      }
+
+      if(i==1) {
+        if(j<8) {
+          chessboard[i].push("Pion Hitam");
+        }
+
+      } else if(i>1 && i<6) {
+        chessboard[i].push(" ");
+      } else if (i==6) {
+        chessboard[i].push("Pion Putih");
+      }
+
+      if (i==7)  {
+        chessboard[i].push(`${bidak[j]} Putih`);
+      }
+    }
+  }
+
   return chessboard
 }
 
 const printBoard = x => {
   // ... write your code here
+  let Rapihkan = makeChessboard();
+  for (let i=0; i<8; i++) {
+    console.log(Rapihkan[i].join("|"));
+  }
 }
 
 printBoard(makeChessboard())
