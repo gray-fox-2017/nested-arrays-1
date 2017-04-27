fddb191bc9ae57cd12925a3392a0520723909006
fajarkarim <fajarabdulkarim@gmail.com>
2017-04-27 12:02:11 +0700

    build the logic

--- chessboard.js
+++ chessboard.js
@@ -4,6 +4,32 @@ const makeChessboard = () => {
   let chessboard = []
 
   // ... write your code here
+  let tempArr = []
+  let finalArr = [];
+  let figure = ["Benteng ", "Kuda ", "Peluncur ", "Raja ", "Ratu ", "Peluncur ", "Kuda", "Benteng"];
+  let blackChess = [];
+  let whiteChess = [];
+  let blackPion = [];
+  let whitePion = [];
+
+  for (let i = 0; i < figure.length; i++) {
+    blackChess.push(figure[i] + "Hitam");
+    whiteChess.push(figure[i] + "Putih");
+  }
+
+  for (let i = 0; i < 8; i++) {
+    if (i === 0) {
+      finalArr.push(blackChess);
+    } else if (i === 1) {
+      finalArr.push(blackPion);
+    } else if (i === 6) {
+      finalArr.push(whitePion);
+    } else if (i === 7) {
+      finalArr.push(whiteChess.reverse());
+    } else {
+      finalArr.push("Kosong");
+    }
+  }
   return chessboard
 }
 
