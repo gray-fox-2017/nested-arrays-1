b4c7f6966a4bdbf333c7b3bc6c818831de0474c5
jumadiakhmad <jumadiakhmad2@gmail.com>
2017-04-27 20:36:12 +0700

    Tictactoe Done

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,34 @@
 function tic_tac_toe_board () {
   // ...
+  let board = [],
+      countX=0,
+      countO=0;
+
+
+ for(var i = 0 ; i < 3 ; i ++){
+   var baris=[];
+    for(var j =0 ; j < 3 ; j ++){
+
+      var random=Math.floor(Math.random()*2);
+   if(random === 0  && countX < 5){
+
+     countX++;
+     baris.push('X');
+   }else if(countO < 5){
+
+     countO++;
+      baris.push('O');
+   }else{
+
+     countX++;
+      baris.push('X' || 'Y');
+   }
+ }
+  board.push(baris);
+ }
+
+ return board;
+
 }
 
-console.log(tic_tac_toe_board()) // => make a random tic tic board
+console.log(tic_tac_toe_board()); // => make a random tic tic board
