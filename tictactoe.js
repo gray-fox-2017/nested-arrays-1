3475487db8ee395ed9b298229ff5b81580e79a17
sidikriders <sidik.riders@gmail.com>
2017-05-02 11:48:09 +0700

    mempercantik aja

--- tictactoe.js
+++ tictactoe.js
@@ -7,18 +7,6 @@ function tic_tac_toe_board () {
   for (let i = 0; i<9; i++) {
     generator();
   }
-  // for (let i = 0; i<4; i++) {
-  //   let gabung = ticTac.join('');
-  //   let jumlahX = gabung.match(/[x]/g);
-  //   let jumlahO = gabung.match(/[o]/g);
-  //   if (jumlahO.length === jumlahX.length) {
-  //     generator();
-  //   } else if (jumlahO.length  < jumlahX.length) {
-  //     ticTac.push('o');
-  //   } else if (jumlahO.length > jumlahX.length ){
-  //     ticTac.push('x');
-  //   }
-  // }
 
   function generator() {
     let rand = Math.floor(Math.random()*10);
@@ -43,10 +31,10 @@ function tic_tac_toe_board () {
 
   var ticTacBoard = [];
   for (i=0; i<ticTac.length;i+=3) {
-    ticTacBoard.push(ticTac.slice(i, i+3))
+    ticTacBoard.push(ticTac.slice(i, i+3).join(' '))
   }
 
-  return ticTacBoard;
+  return ticTacBoard.join('\n');
 }
 
 
